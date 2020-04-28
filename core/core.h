a194bad4c178c53d7254058eec3e3ba0d3459f9c
Simon Everts <simoneverts@gmail.com>
2020-04-28 16:48:02 +0200

    add system-time to stopwatch for trace logging (#10)
    * add system-time to stopwatch for trace logging
    
    * fix compile issue on ubuntu-1804 gcc-8
    
    Co-authored-by: Simon Everts <ses@sanovogroup.com>

--- core/core.h
+++ core/core.h
@@ -24,6 +24,10 @@ namespace outcome = OUTCOME_V2_NAMESPACE;
 
 #include <gsl/span>
 
+#include <cstdio>
+#include <cstdlib>
+#include <string_view>
+
 namespace katla {
 
     template<class T, std::size_t Extent>
@@ -40,6 +44,12 @@ namespace katla {
     template <typename S, typename... Args>
     inline void print(std::FILE* f, const S& format_str, Args&&... args) {
         fmt::print(f, format_str, args...);
+    }
+
+    template <typename S>
+    inline void fatal(const S& message) {
+        print(stderr, "{}", message);
+        std::abort();
     }
 }
 
