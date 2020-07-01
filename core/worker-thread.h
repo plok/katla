9687641099aa4ee7df7a1f4ca98a4673a794157a
Simon Everts <simoneverts@gmail.com>
2020-07-01 15:51:37 +0200

    add worker-thread (#23)
    Co-authored-by: Simon Everts <ses@sanovogroup.com>

---
+++ core/worker-thread.h
@@ -0,0 +1,62 @@
+/***
+ * Copyright 2019 The Katla Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef KATLA_WORKER_THREAD_H
+#define KATLA_WORKER_THREAD_H
+
+#include "katla/core/core.h"
+#include "katla/core/posix-thread.h"
+
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <thread>
+
+namespace katla {
+
+class WorkerThread {
+  public:
+    WorkerThread(std::string name, katla::PosixThread::Priority priority);
+    virtual ~WorkerThread();
+
+    outcome::result<void, Error> init(std::function<bool(void)> repeatableWork, std::chrono::milliseconds interval);
+
+    void wakeup();
+
+    void stop();
+    void join();
+
+  private:
+    void exec(const std::function<bool(void)>& repeatableWork);
+
+    std::unique_ptr<std::thread> m_thread;
+    std::mutex m_mutex;
+    std::condition_variable m_condition;
+
+    std::string m_name { "WorkerThread" };
+
+    katla::PosixThread::Priority m_priority { katla::PosixThread::Priority::Normal };
+
+    bool m_stop {};
+    std::chrono::milliseconds m_interval { 1000 };
+};
+
+} // namespace katla
+
+#endif
