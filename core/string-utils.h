510324bb4a01b488e1ad8accb85f598e87f52a2e
Simon Everts <simoneverts@gmail.com>
2021-11-07 17:07:43 +0100

    add string-utils for human-readable sizes and prefix utils (#51)
    * add string-utils for human-readable sizes and prefix utils

---
+++ core/string-utils.h
@@ -0,0 +1,42 @@
+/***
+ * Copyright 2019 The Katla Authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef STRING_UTILS_H
+#define STRING_UTILS_H
+
+#include "core/core.h"
+
+#ifdef _MSC_VER
+    #ifdef KATLA_CORE_INDLL
+    # define KATLA_CORE_DECLSPEC __declspec(dllexport)
+    #else
+    # define KATLA_CORE_DECLSPEC __declspec(dllimport)
+    #endif
+#else
+    #define KATLA_CORE_DECLSPEC
+#endif
+
+namespace katla {
+namespace string {
+
+    std::string humanFileSize(long long fileSize);
+
+    std::string trimPrefix(std::string src, std::string prefix);
+    
+}}
+
+
+#endif // KATLA_CORE_H
